e11853b266476c98ac98eec3d72ee47fb8df2269
fatsushi <fatsushi@amazon.co.jp>
2019-01-27 22:52:56 +0900

    modify container name

--- lib/fargate-cicd-env-stack.ts
+++ lib/fargate-cicd-env-stack.ts
@@ -3,10 +3,6 @@ import ec2 = require('@aws-cdk/aws-ec2');
 import ecs = require('@aws-cdk/aws-ecs');
 import { SubnetType } from '@aws-cdk/aws-ec2';
 
-//import { AvailabilityZoneProvider } from '@aws-cdk/cdk';
-//import s3 = require('@aws-cdk/aws-s3');
-//import { SubnetType } from '@aws-cdk/aws-ec2';
-
 export class FargateCicdEnvStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);    
@@ -36,9 +32,9 @@ export class FargateCicdEnvStack extends cdk.Stack {
     new ecs.LoadBalancedFargateService(this, 'MyFargateService', {
       cluster: cluster,  // Required
       cpu: '512', // Default is 256
-      desiredCount: 2,　//Default is 1
+      desiredCount: 2, //Default is 1
       image: ecs.ContainerImage.fromDockerHub('amazon/amazon-ecs-sample'), // Required
-      memoryMiB: '1GB',  // Default is 512
+      memoryMiB: '1GB', // Default is 512
       publicLoadBalancer: true,
     });    //create aws resources
   }
